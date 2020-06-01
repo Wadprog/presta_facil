5d67f140a45e5374dfe4967fe266f9278dd8ef9f
Александр Творогов <tvorogov100500@gmail.com>
2020-06-01 14:41:48 +0300

    fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,72 @@
 import React from 'react';
-
+import { StaticQuery, graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 import Layout from '@components/Layout';
-// import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    {/* <SEO title="404: Not found" /> */}
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+const NotFoundPage = ({ data }) => {
+  return (
+    <Layout data={data}>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </Layout>
+  );
+};
 
-export default NotFoundPage;
+NotFoundPage.propTypes = {
+  data: PropTypes.object.isRequired,
+};
+const query = graphql`
+  {
+    prismic {
+      allLayouts {
+        edges {
+          node {
+            body {
+              ... on PRISMIC_LayoutBodyHeader {
+                type
+                label
+                primary {
+                  slogan
+                  buttontext
+                  logo
+                }
+              }
+            }
+            body1 {
+              ... on PRISMIC_LayoutBody1Footer {
+                type
+                label
+                primary {
+                  buttontitle
+                  buttontext
+                  bookstitle
+                  copyright
+                }
+                fields {
+                  socialogo
+                  sociallink {
+                    ... on PRISMIC__ExternalLink {
+                      _linkType
+                      url
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const PageWithData = () => {
+  return (
+    <StaticQuery
+      query={`${query}`}
+      render={(data) => <NotFoundPage data={data} />}
+    />
+  );
+};
+
+export default PageWithData;
