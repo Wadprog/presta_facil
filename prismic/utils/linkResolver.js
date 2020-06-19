7b1d85bc786add18a74008967a1e83547edde1c8
Александр Творогов <tvorogov100500@gmail.com>
2020-06-19 17:26:10 +0300

    add link resolver

--- prismic/utils/linkResolver.js
+++ prismic/utils/linkResolver.js
@@ -3,6 +3,9 @@ export const linkResolver = (doc) => {
   if (doc.type === 'solutionpage') {
     return `/solution/${doc.uid}`;
   }
+  if (doc.type === 'technologypage') {
+    return `/technology/${doc.uid}`;
+  }
   // Backup for all other types
   return '/';
 };
