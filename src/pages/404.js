ccafa6f3628723f9656d7a7535b89aae5d7820cf
Александр Творогов <tvorogov100500@gmail.com>
2020-07-01 12:17:59 +0300

    404 page create

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Layout from '@components/Layout';
+import ErrorPage from '../scenes/ErrorPage/ErrorPage';
 
 const NotFoundPage = () => {
   return (
     <Layout>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <ErrorPage />
     </Layout>
   );
 };
