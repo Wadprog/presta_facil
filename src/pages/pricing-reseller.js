415720b53909bbfa57c9bc05ae7eafeb40c15ca6
Mikhail Netkachev <mikhailnetkachev@MacBook-Pro-Mikhail.local>
2020-08-03 08:53:21 +0300

    Edited ReselletPage: connected to content model.

--- src/pages/pricing-reseller.js
+++ src/pages/pricing-reseller.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
-import ResellerPage from '@scenes/ResellerPage';
 import Layout from '@components/Layout';
+import ResellerPage from '@scenes/ResellerPage';
 
 const Page = ({ data }) => {
   return (
@@ -15,5 +16,40 @@ const Page = ({ data }) => {
 Page.propTypes = {
   data: PropTypes.object.isRequired,
 };
+
+export const query = graphql`
+  {
+    prismic {
+      allPricesresellerpages {
+        edges {
+          node {
+            body {
+              ... on PRISMIC_PricesresellerpageBodyHero {
+                type
+                label
+                primary {
+                  title
+                  description
+                  buttonlink
+                  buttontext
+                  modalvideo {
+                    ... on PRISMIC__ExternalLink {
+                      _linkType
+                      target
+                      url
+                    }
+                  }
+                }
+                fields {
+                  partnerslogo
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default Page;
--- src/scenes/ResellerPage/ResellerPage.js
+++ src/scenes/ResellerPage/ResellerPage.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import style from './ResellerPage.module.scss';
+import Hero from '@components/Hero';
 
-const ResellerPage = () => {
-  return <div className={style.ResellerPage}>ResellerPage</div>;
+const ResellerPage = ({ content }) => {
+  const body = content.prismic.allPricesresellerpages.edges[0].node.body;
+  const sections = body.map((item, index) => {
+    switch (item.type) {
+      case 'hero':
+        return <Hero key={index} {...item} />;
+      default:
+        return null;
+    }
+  });
+
+  return (
+    <div className={style.container}>
+      {/* <div className={style.wrapper}>{sections}</div> */}
+    </div>
+  );
+};
+
+ResellerPage.propTypes = {
+  content: PropTypes.object.isRequired,
 };
 
 export default ResellerPage;
