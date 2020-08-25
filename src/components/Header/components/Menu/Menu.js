4abb251adf068552b0af7e7dd6d395cbb386d8d1
Aleksandr-Tyagun <alexander.tiagun@gmail.com>
2020-08-26 00:11:12 +0300

    fixed menu double select bug

--- src/components/Header/components/Menu/Menu.js
+++ src/components/Header/components/Menu/Menu.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Menu.module.scss';
 import { array, bool } from 'prop-types';
 import classnames from 'classnames';
 import MenuItem from '../MenuItem/MenuItem';
 
 const Menu = ({ data, open }) => {
+  const [activeMenu, setActiveMenu] = useState('');
   const menuclass = classnames({
     [style.menu]: true,
     [style.open]: open,
   });
+
+  const handleActiveMenu = (title = '') => {
+    if (title === activeMenu) {
+      setActiveMenu('');
+    } else {
+      setActiveMenu(title);
+    }
+  };
+
   return (
     <menu className={menuclass}>
       {data.map((item, index) => {
         if (item.type === 'menu') {
-          return <MenuItem {...item} key={index} />;
+          return (
+            <MenuItem
+              {...item}
+              key={index}
+              activeMenu={activeMenu}
+              handleActiveMenu={handleActiveMenu}
+            />
+          );
         }
       })}
     </menu>
