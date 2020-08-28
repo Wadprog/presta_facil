f99cf24d2580cc44101b24957db01d6755edb069
aleksandr-tyagun <alexander.tiagun@gmail.com>
2020-08-28 18:04:17 +0300

    Added styles

--- src/scenes/ResellerPage/components/Testimonials/components/Reviews/Reviews.js
+++ src/scenes/ResellerPage/components/Testimonials/components/Reviews/Reviews.js
@@ -10,6 +10,13 @@ const params = {
   spaceBetween: 16,
 };
 
+const params2 = {
+  slidesPerView: 3,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
+  grabCursor: true,
+};
+
 const Reviews = ({
   items,
   buttonText,
@@ -17,29 +24,150 @@ const Reviews = ({
   buttonLink,
   logotype,
 }) => {
+  const items2 = [
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Alexandre G',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The fact that we can track all user opt-in and opt-out data and review it later to ensure compliance with LGPD cookie consent compliance requirements.',
+          spans: [],
+        },
+      ],
+    },
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Francis U',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The automated web scanning has been really resourceful for us in identifying the cookies on our website and managing them in line with GDPR...',
+          spans: [],
+        },
+      ],
+    },
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Jagrati S',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The simplicity and clarity around Secure Privacy’s sync consent across multiple website domains capability gave us the structure we needed to meet...',
+          spans: [],
+        },
+      ],
+    },
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Maša K',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The ‘Prior Consent’ tool that blocks the installation of unnecessary cookies on users’ computers until they give consent. This is very important to us in our efforts...',
+          spans: [],
+        },
+      ],
+    },
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Maša K',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The ‘Prior Consent’ tool that blocks the installation of unnecessary cookies on users’ computers until they give consent. This is very important to us in our efforts...',
+          spans: [],
+        },
+      ],
+    },
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Maša K',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The ‘Prior Consent’ tool that blocks the installation of unnecessary cookies on users’ computers until they give consent. This is very important to us in our efforts...',
+          spans: [],
+        },
+      ],
+    },
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Maša K',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The ‘Prior Consent’ tool that blocks the installation of unnecessary cookies on users’ computers until they give consent. This is very important to us in our efforts...',
+          spans: [],
+        },
+      ],
+    },
+    {
+      author: [
+        {
+          type: 'paragraph',
+          text: 'Maša K',
+          spans: [],
+        },
+      ],
+      text: [
+        {
+          type: 'paragraph',
+          text:
+            'The ‘Prior Consent’ tool that blocks the installation of unnecessary cookies on users’ computers until they give consent. This is very important to us in our efforts...',
+          spans: [],
+        },
+      ],
+    },
+  ];
   return (
     <div className={style.container}>
-      <div className={style.slider}>
-        <Swiper {...params}>
-          {items.map((item, index) => {
-            return (
-              <div key={index} className={style.slide}>
-                <Card
-                  {...item}
-                  buttonText={buttonText}
-                  buttonTextShort={buttonTextShort}
-                  buttonLink={buttonLink}
-                  logotype={logotype}
-                />
-              </div>
-            );
-          })}
-        </Swiper>
-      </div>
-      <ul className={style.list}>
-        {items.map((item, index) => {
+      <Swiper {...params2}>
+        {items2.map((item, index) => {
           return (
-            <li key={index} className={style.item}>
+            <div className={style.item} key={index}>
               <Card
                 {...item}
                 buttonText={buttonText}
@@ -47,10 +175,10 @@ const Reviews = ({
                 buttonLink={buttonLink}
                 logotype={logotype}
               />
-            </li>
+            </div>
           );
         })}
-      </ul>
+      </Swiper>
     </div>
   );
 };
