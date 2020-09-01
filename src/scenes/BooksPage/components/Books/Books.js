ff80ed5c74086f633301f90955315b76d44561a5
aleksandr-tyagun <alexander.tiagun@gmail.com>
2020-09-01 18:16:25 +0300

    Book Page styles were changed

--- src/scenes/BooksPage/components/Books/Books.js
+++ src/scenes/BooksPage/components/Books/Books.js
@@ -1,58 +1,29 @@
 import React from 'react';
 import { array } from 'prop-types';
-import Button, { VARIANT } from '@components/Button/Button.js';
 import style from './Books.module.scss';
-import Swiper from 'react-id-swiper';
-import Image from '@components/Image/Image';
-import { parseString } from '@helpers';
 
 const Books = ({ fields }) => {
-  const params = {
-    slidesPerView: 'auto',
-    spaceBetween: 16,
-    centeredSlides: true,
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    breakpoints: {
-      768: {
-        spaceBetween: 50,
-        slidesPerView: 3,
-        initialSlide: 1,
-        centeredSlides: true,
-      },
-    },
-  };
   return (
     <div className={style.page}>
       <div className={style.container}>
-        <Swiper {...params}>
-          {fields.map((item) => {
-            const { image, imageSharp, buttontext, downloadlink, flag } = item;
-            return (
-              <div className={style.slide} key={downloadlink.url}>
-                <div className={style.item}>
-                  <Image image={flag} className={style.flag} />
-                  <div className={style.imageWrapper}>
-                    <Image image={image} imageSharp={imageSharp} />
-                  </div>
-                  <div className={style.buttonWrapper}>
-                    <Button
-                      variant={VARIANT.PRIMARY}
-                      fullWidth
-                      to={downloadlink.url}
-                      element="external"
-                    >
-                      {parseString(buttontext)}
-                    </Button>
-                  </div>
+        {fields.map(({ downloadlink }) => (
+          <div className={style.book} key={downloadlink.url}>
+            <div className={style.wrapper}>
+              <div className={style.header}>
+                <div className={style.title}>What is LGPD</div>
+                <div className={style.subtitle}>
+                  and how to make your website compliant
                 </div>
               </div>
-            );
-          })}
-        </Swiper>
+              <div className={style.footer}>
+                <div className={style.desc}>
+                  Learn about the LGPD and how to become compliant
+                </div>
+              </div>
+              <div className={style.download}>Download</div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
