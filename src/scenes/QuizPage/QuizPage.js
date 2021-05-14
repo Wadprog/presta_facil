22af83ee788688e3f9100f099b59ec763851f006
MityaDementiy <d.dementiy@gmail.com>
2021-05-14 12:52:41 +0300

    Add h1 and correct meta data on Quiz page

--- src/scenes/QuizPage/QuizPage.js
+++ src/scenes/QuizPage/QuizPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { RichText } from 'prismic-reactjs';
+import PropTypes from 'prop-types';
+
 import style from './QuizPage.module.scss';
 
-const QuizPage = () => {
+const QuizPage = ({ pagetitle }) => {
   return (
     <div className={style.quizPage}>
       <div className={style.container}>
+        <div className={style.headlineContainer}>
+          <RichText render={pagetitle} />
+        </div>
         <iframe
           className={style.frame}
           src="https://www.videoask.com/frwmkcwhp"
@@ -14,6 +20,10 @@ const QuizPage = () => {
       </div>
     </div>
   );
+};
+
+QuizPage.propTypes = {
+  pagetitle: PropTypes.array.isRequired,
 };
 
 export default QuizPage;
