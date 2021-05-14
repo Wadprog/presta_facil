4388e87bb034bb5a4fe0824959cc56dfc48f79d0
MityaDementiy <d.dementiy@gmail.com>
2021-05-14 11:23:39 +0300

    Add h1 on books page

--- src/scenes/BooksPage/components/Books/Books.js
+++ src/scenes/BooksPage/components/Books/Books.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { array } from 'prop-types';
+import { RichText } from 'prismic-reactjs';
+
 import { parseString } from '@helpers';
 import Image from '@components/Image/Image';
-
 import Arrow from './image/arrow.inline.svg';
 import style from './Books.module.scss';
 
-const Books = ({ fields }) => {
+const Books = ({ fields, pagetitle }) => {
   return (
     <div className={style.page}>
+      <div className={style.headlineContainer}>
+        <RichText render={pagetitle} />
+      </div>
       <div className={style.container}>
         {fields.map(({ image, imageSharp, buttontext, bookpageurl }) => (
           <a
@@ -32,6 +36,7 @@ const Books = ({ fields }) => {
 
 Books.propTypes = {
   fields: array,
+  pagetitle: array,
 };
 
 export default Books;
