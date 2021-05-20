048281ddf5327afac90d2bcbad40cfdad50a3a55
MityaDementiy <d.dementiy@gmail.com>
2021-05-20 11:02:26 +0300

    Defer offscreen images by lazy loading

--- src/scenes/PostPage/components/Text/Text.js
+++ src/scenes/PostPage/components/Text/Text.js
@@ -3,8 +3,25 @@ import style from './Text.module.scss';
 import { RichText } from 'prismic-reactjs';
 import { object } from 'prop-types';
 
+const propsWithUniqueKey = (props, key) => {
+  return Object.assign(props || {}, { key });
+};
+
+const htmlSerializer = (type, element, key) => {
+  if (type !== 'image') {
+    return;
+  }
+
+  const props = { src: element.url, alt: element.alt || '', loading: 'lazy' };
+  return React.createElement('img', propsWithUniqueKey(props, key));
+};
+
 const Text = ({ primary }) => {
-  return <div className={style.text}>{<RichText render={primary.text} />}</div>;
+  return (
+    <div className={style.text}>
+      {<RichText render={primary.text} htmlSerializer={htmlSerializer} />}
+    </div>
+  );
 };
 
 Text.propTypes = {
