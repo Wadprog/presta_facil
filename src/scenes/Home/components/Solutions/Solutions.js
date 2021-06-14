b664754470ded73c84c768b78d5a90ea044407cf
MityaDementiy <d.dementiy@gmail.com>
2021-06-14 18:32:49 +0300

    Optimize background image in Solutions section on Homepage

--- src/scenes/Home/components/Solutions/Solutions.js
+++ src/scenes/Home/components/Solutions/Solutions.js
@@ -3,7 +3,7 @@ import { RichText } from 'prismic-reactjs';
 import { array, object } from 'prop-types';
 import { Link } from 'gatsby';
 import Swiper from 'react-id-swiper';
-import lozad from 'lozad';
+import BackgroundImage from 'gatsby-background-image';
 
 import style from './Solutions.module.scss';
 import useGetImage from './useGetImage';
@@ -11,21 +11,18 @@ import { useBreakpoints } from '@hooks';
 import LangContext from '@contexts';
 import { langPath } from '@helpers';
 import Image from '@components/Image/Image';
+import useGetEarthImage from './useGetEarthImage';
 
 const Solutions = ({ primary, fields }) => {
   const [buildKey, setBuildKey] = useState();
   const { width } = useBreakpoints();
   const { arrow } = useGetImage();
   const currentLang = useContext(LangContext);
+  const { background } = useGetEarthImage();
 
   useEffect(() => {
     setBuildKey(+new Date());
   }, [width]);
-
-  useEffect(() => {
-    const observer = lozad();
-    observer.observe();
-  }, []);
 
   const params = {
     slidesPerView: 'auto',
@@ -42,40 +39,48 @@ const Solutions = ({ primary, fields }) => {
   };
 
   return (
-    <div className={style.solutions}>
-      <div className={style.container}>
-        <div className={style.title}>
-          <RichText render={primary.title} />
-        </div>
-        <div className={style.descr}>
-          <RichText render={primary.description} />
-        </div>
-        <Swiper {...params} key={buildKey}>
-          {fields.map(({ image, imageSharp, title, text, pagename }, index) => {
-            const link = `${langPath(currentLang)}/solution/${RichText.asText(
-              pagename
-            )}`;
-            return (
-              <div className={style.slide} key={`solutions${index}`}>
-                <Link to={link} className={style.item}>
-                  <Image image={image} imageSharp={imageSharp} />
-                  <RichText render={title} />
-                  <div>
-                    (<RichText render={text} />)
+    <BackgroundImage
+      fluid={background.childImageSharp.fluid}
+      className={style.background}
+    >
+      <div className={style.solutions}>
+        <div className={style.container}>
+          <div className={style.title}>
+            <RichText render={primary.title} />
+          </div>
+          <div className={style.descr}>
+            <RichText render={primary.description} />
+          </div>
+          <Swiper {...params} key={buildKey}>
+            {fields.map(
+              ({ image, imageSharp, title, text, pagename }, index) => {
+                const link = `${langPath(
+                  currentLang
+                )}/solution/${RichText.asText(pagename)}`;
+                return (
+                  <div className={style.slide} key={`solutions${index}`}>
+                    <Link to={link} className={style.item}>
+                      <Image image={image} imageSharp={imageSharp} />
+                      <RichText render={title} />
+                      <div>
+                        (<RichText render={text} />)
+                      </div>
+                      <img
+                        className={style.arrow}
+                        src={arrow.publicURL}
+                        alt="arrow icon"
+                        loading="lazy"
+                      />
+                      <div className={style.shadow}></div>
+                    </Link>
                   </div>
-                  <img
-                    className={`${style.arrow} lozad`}
-                    data-src={arrow.publicURL}
-                    alt="arrow icon"
-                  />
-                  <div className={style.shadow}></div>
-                </Link>
-              </div>
-            );
-          })}
-        </Swiper>
+                );
+              }
+            )}
+          </Swiper>
+        </div>
       </div>
-    </div>
+    </BackgroundImage>
   );
 };
 
