696a8e96e9b8e3819b8f790ad3daaa99b8548f58
MityaDementiy <d.dementiy@gmail.com>
2021-06-14 17:19:42 +0300

    Optimize background image on Books page

--- src/scenes/BooksPage/components/Books/Books.js
+++ src/scenes/BooksPage/components/Books/Books.js
@@ -1,36 +1,45 @@
 import React from 'react';
 import { array } from 'prop-types';
 import { RichText } from 'prismic-reactjs';
+import BackgroundImage from 'gatsby-background-image';
 
 import { parseString } from '@helpers';
 import Image from '@components/Image/Image';
 import Arrow from './image/arrow.inline.svg';
 import style from './Books.module.scss';
+import useGetImages from './useGetImages';
 
 const Books = ({ fields, pagetitle }) => {
+  const { background } = useGetImages();
+
   return (
-    <div className={style.page}>
-      <div className={style.headlineContainer}>
-        <RichText render={pagetitle} />
-      </div>
-      <div className={style.container}>
-        {fields.map(({ image, imageSharp, buttontext, bookpageurl }) => (
-          <a
-            href={parseString(bookpageurl)}
-            className={style.book}
-            key={parseString(bookpageurl)}
-          >
-            <Image image={image} imageSharp={imageSharp} />
-            <div className={style.buttonWrapper}>
-              <div className={style.button}>
-                {parseString(buttontext)}
-                <Arrow />
+    <BackgroundImage
+      fluid={background.childImageSharp.fluid}
+      className={style.background}
+    >
+      <div className={style.page}>
+        <div className={style.headlineContainer}>
+          <RichText render={pagetitle} />
+        </div>
+        <div className={style.container}>
+          {fields.map(({ image, imageSharp, buttontext, bookpageurl }) => (
+            <a
+              href={parseString(bookpageurl)}
+              className={style.book}
+              key={parseString(bookpageurl)}
+            >
+              <Image image={image} imageSharp={imageSharp} />
+              <div className={style.buttonWrapper}>
+                <div className={style.button}>
+                  {parseString(buttontext)}
+                  <Arrow />
+                </div>
               </div>
-            </div>
-          </a>
-        ))}
+            </a>
+          ))}
+        </div>
       </div>
-    </div>
+    </BackgroundImage>
   );
 };
 
