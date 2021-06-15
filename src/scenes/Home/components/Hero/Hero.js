428a7bdbbdf1242914ff80e5f7193b18c03ba866
MityaDementiy <d.dementiy@gmail.com>
2021-06-15 11:11:37 +0300

    Refactor background image in Hero section on Home page

--- src/scenes/Home/components/Hero/Hero.js
+++ src/scenes/Home/components/Hero/Hero.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { object, array } from 'prop-types';
 import { RichText } from 'prismic-reactjs';
 import Swiper from 'react-id-swiper';
-import BackgroundImage from 'gatsby-background-image';
 
 import { parseString } from '@helpers';
 import Button, { VARIANT } from '@components/Button/Button.js';
@@ -12,7 +11,6 @@ import Image from '@components/Image/Image';
 import styles from './Hero.module.scss';
 import PLayIcon from '@src/assets/images/homepage/icons/play.inline.svg';
 import ModalBookCall from '@components/ModalBookCall/ModalBookCall';
-import useGetImages from './useGetImages';
 
 const renderMobileImages = (images) => {
   const renderedImages = images.map((imgElement, index) => (
@@ -33,7 +31,7 @@ const Hero = ({ primary, fields }) => {
   const buttonLink = primary.buttonlink[0].text;
   const modalCtaButtonLink = parseString(primary.modalctabuttonlink);
   const modalCtaButtonText = parseString(primary.modalctabuttontext);
-  const { background } = useGetImages();
+
   const {
     cookieimage,
     cookieimageSharp,
@@ -66,83 +64,75 @@ const Hero = ({ primary, fields }) => {
   };
 
   return (
-    <BackgroundImage
-      fluid={background.childImageSharp.fluid}
-      className={styles.background}
-    >
-      <div className={styles.hero}>
-        <div className={styles.container}>
-          <div className={styles.content}>
-            <div className={styles.mobileImagesWrapper}>
-              {renderMobileImages(mobileImages)}
-            </div>
-            <div className={styles.upTitle}>
-              <RichText render={primary.sub_title} />
-            </div>
-            <div className={styles.title}>
-              <RichText render={primary.title} />
-            </div>
-            <div className={styles.descr}>
-              <RichText render={primary.description} />
-            </div>
-            <div className={styles.buttonWrapper}>
-              <Button variant={VARIANT.PRIMARY} to={`${buttonLink}`}>
-                <RichText render={primary.button} />
-              </Button>
-            </div>
-            <div className={styles.trustedWrapper}>
-              <RichText render={primary.trusted} />
-              <div className={styles.companies}>
-                <Swiper {...params}>
-                  {fields.map(({ trustedlogo }) => {
-                    return (
-                      <div className={styles.slide} key={trustedlogo.url}>
-                        <Image
-                          image={trustedlogo}
-                          className={styles.companyLogo}
-                        />
-                      </div>
-                    );
-                  })}
-                </Swiper>
-              </div>
-            </div>
+    <div className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.mobileImagesWrapper}>
+            {renderMobileImages(mobileImages)}
           </div>
-          <div className={styles.imageWrapper}>
-            <Image
-              image={primary.mainImage}
-              imageSharp={primary.mainImageSharp}
-              className={styles.image}
-            />
-            <div className={styles.playButtonWrapper}>
-              <div className={styles.playButton}>
-                <IconButton
-                  isGradient={true}
-                  variant={VARIANT_ICON.PLAY}
-                  click={handleOpenModal}
-                >
-                  <PLayIcon />
-                </IconButton>
-              </div>
-              <div className={styles.playButtonText}>
-                <RichText render={primary.modalbuttontitle} />
-              </div>
+          <div className={styles.upTitle}>
+            <RichText render={primary.sub_title} />
+          </div>
+          <div className={styles.title}>
+            <RichText render={primary.title} />
+          </div>
+          <div className={styles.descr}>
+            <RichText render={primary.description} />
+          </div>
+          <div className={styles.buttonWrapper}>
+            <Button variant={VARIANT.PRIMARY} to={`${buttonLink}`}>
+              <RichText render={primary.button} />
+            </Button>
+          </div>
+          <div className={styles.trustedWrapper}>
+            <RichText render={primary.trusted} />
+            <div className={styles.companies}>
+              <Swiper {...params}>
+                {fields.map(({ trustedlogo }) => {
+                  return (
+                    <div className={styles.slide} key={trustedlogo.url}>
+                      <Image
+                        image={trustedlogo}
+                        className={styles.companyLogo}
+                      />
+                    </div>
+                  );
+                })}
+              </Swiper>
             </div>
           </div>
         </div>
-        <Modal
-          open={modalIsOpen}
-          closeModal={handleCloseModal}
-          videoLink={primary.modalvideo.url}
-          modalCtaButtonLink={modalCtaButtonLink}
-          modalCtaButtonText={modalCtaButtonText}
-        />
-        <ModalBookCall
-          open={modalBookIsOpen}
-          closeModal={handleCloseModalBook}
-        />
+        <div className={styles.imageWrapper}>
+          <Image
+            image={primary.mainImage}
+            imageSharp={primary.mainImageSharp}
+            className={styles.image}
+          />
+          <div className={styles.playButtonWrapper}>
+            <div className={styles.playButton}>
+              <IconButton
+                isGradient={true}
+                variant={VARIANT_ICON.PLAY}
+                click={handleOpenModal}
+              >
+                <PLayIcon />
+              </IconButton>
+            </div>
+            <div className={styles.playButtonText}>
+              <RichText render={primary.modalbuttontitle} />
+            </div>
+          </div>
+        </div>
       </div>
-    </BackgroundImage>
+      <Modal
+        open={modalIsOpen}
+        closeModal={handleCloseModal}
+        videoLink={primary.modalvideo.url}
+        modalCtaButtonLink={modalCtaButtonLink}
+        modalCtaButtonText={modalCtaButtonText}
+      />
+      <ModalBookCall open={modalBookIsOpen} closeModal={handleCloseModalBook} />
+    </div>
   );
 };
 
