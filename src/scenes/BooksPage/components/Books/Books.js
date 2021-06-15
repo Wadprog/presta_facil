3394499330155676dfaae94074ccb80a5d4f3cbb
MityaDementiy <d.dementiy@gmail.com>
2021-06-15 12:02:51 +0300

    Refactor background images in Hero section on Enterprise and Books pages

--- src/scenes/BooksPage/components/Books/Books.js
+++ src/scenes/BooksPage/components/Books/Books.js
@@ -1,45 +1,36 @@
 import React from 'react';
 import { array } from 'prop-types';
 import { RichText } from 'prismic-reactjs';
-import BackgroundImage from 'gatsby-background-image';
 
 import { parseString } from '@helpers';
 import Image from '@components/Image/Image';
 import Arrow from './image/arrow.inline.svg';
 import style from './Books.module.scss';
-import useGetImages from './useGetImages';
 
 const Books = ({ fields, pagetitle }) => {
-  const { background } = useGetImages();
-
   return (
-    <BackgroundImage
-      fluid={background.childImageSharp.fluid}
-      className={style.background}
-    >
-      <div className={style.page}>
-        <div className={style.headlineContainer}>
-          <RichText render={pagetitle} />
-        </div>
-        <div className={style.container}>
-          {fields.map(({ image, imageSharp, buttontext, bookpageurl }) => (
-            <a
-              href={parseString(bookpageurl)}
-              className={style.book}
-              key={parseString(bookpageurl)}
-            >
-              <Image image={image} imageSharp={imageSharp} />
-              <div className={style.buttonWrapper}>
-                <div className={style.button}>
-                  {parseString(buttontext)}
-                  <Arrow />
-                </div>
-              </div>
-            </a>
-          ))}
-        </div>
+    <div className={style.page}>
+      <div className={style.headlineContainer}>
+        <RichText render={pagetitle} />
       </div>
-    </BackgroundImage>
+      <div className={style.container}>
+        {fields.map(({ image, imageSharp, buttontext, bookpageurl }) => (
+          <a
+            href={parseString(bookpageurl)}
+            className={style.book}
+            key={parseString(bookpageurl)}
+          >
+            <Image image={image} imageSharp={imageSharp} />
+            <div className={style.buttonWrapper}>
+              <div className={style.button}>
+                {parseString(buttontext)}
+                <Arrow />
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
   );
 };
 
