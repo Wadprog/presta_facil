bf9f9d47b84082a46b4a2c4596bad0c955057741
MityaDementiy <d.dementiy@gmail.com>
2021-06-15 11:46:44 +0300

    Refactor background images in Hero section on Technology page

--- src/scenes/TechnologyPage/components/Hero/Hero.js
+++ src/scenes/TechnologyPage/components/Hero/Hero.js
@@ -1,48 +1,40 @@
 import React from 'react';
 import { object, array } from 'prop-types';
 import { RichText } from 'prismic-reactjs';
-import BackgroundImage from 'gatsby-background-image';
 
 import { parseString } from '@helpers';
 import Button, { VARIANT } from '@components/Button/Button.js';
 import Image from '@components/Image/Image';
 import styles from './Hero.module.scss';
-import useGetImages from './useGetImages';
 
 const Hero = ({ primary }) => {
   const buttonLink = parseString(primary.buttonlink);
-  const { background } = useGetImages();
 
   return (
-    <BackgroundImage
-      fluid={background.childImageSharp.fluid}
-      className={styles.background}
-    >
-      <div className={styles.hero}>
-        <div className={styles.container}>
-          <div className={styles.content}>
-            <div className={styles.title}>
-              <RichText render={primary.title} />
-            </div>
-            <div className={styles.descr}>
-              <RichText render={primary.description} />
-            </div>
-            <div className={styles.buttonWrapper}>
-              <Button variant={VARIANT.PRIMARY} to={buttonLink}>
-                <RichText render={primary.buttontext} />
-              </Button>
-            </div>
+    <div className={styles.hero}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <RichText render={primary.title} />
           </div>
-          <div className={styles.imageWrapper}>
-            <Image
-              className={styles.image}
-              imageSharp={primary.imageSharp}
-              image={primary.image}
-            />
+          <div className={styles.descr}>
+            <RichText render={primary.description} />
+          </div>
+          <div className={styles.buttonWrapper}>
+            <Button variant={VARIANT.PRIMARY} to={buttonLink}>
+              <RichText render={primary.buttontext} />
+            </Button>
           </div>
         </div>
+        <div className={styles.imageWrapper}>
+          <Image
+            className={styles.image}
+            imageSharp={primary.imageSharp}
+            image={primary.image}
+          />
+        </div>
       </div>
-    </BackgroundImage>
+    </div>
   );
 };
 
