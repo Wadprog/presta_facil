19db1a20f19ed59b80e4e5aceaed61c2a23969fe
MityaDementiy <d.dementiy@gmail.com>
2021-07-08 13:55:00 +0300

    Load SP script sync

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -6,7 +6,6 @@ export const onRenderBody = ({ setHeadComponents }) => {
       key="sp-app"
       type="text/javascript"
       src="https://app.secureprivacy.ai/script/606acb2d5761b5f013b48067.js"
-      async
     ></script>,
   ]);
 };
