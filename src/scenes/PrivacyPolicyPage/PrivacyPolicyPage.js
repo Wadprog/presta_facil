dccd41aa5ad58fbfd74a60f2c6298b8b8623821f
MityaDementiy <d.dementiy@gmail.com>
2021-07-14 17:37:40 +0300

    Implement markup on privacypolicy page

--- src/scenes/PrivacyPolicyPage/PrivacyPolicyPage.js
+++ src/scenes/PrivacyPolicyPage/PrivacyPolicyPage.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import BreadcrumbsSemanticMarkup from '@components/BreadcrumbsMarkup/BreadcrumbsMarkup';
 import style from './PrivacyPolicyPage.module.scss';
 
-const PrivacyPolicyPage = ({ pageTitle }) => {
+const PrivacyPolicyPage = ({ pageTitle, canonical }) => {
   return (
     <section className={style.PrivacyPolicyPage}>
       <div className={style.container}>
         <h1 className={style.header}>{pageTitle.text}</h1>
         <div sp-privacy-policy-text="direct-text" sp-lang-code="en"></div>
+        <BreadcrumbsSemanticMarkup
+          pageTitle={pageTitle.text}
+          pageUrl={canonical.text}
+        />
       </div>
     </section>
   );
@@ -16,6 +21,7 @@ const PrivacyPolicyPage = ({ pageTitle }) => {
 
 PrivacyPolicyPage.propTypes = {
   pageTitle: PropTypes.object.isRequired,
+  canonical: PropTypes.object.isRequired,
 };
 
 export default PrivacyPolicyPage;
