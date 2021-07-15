6ff5750ebd60a9e30c56d9e10dd85c6c465c1843
MityaDementiy <d.dementiy@gmail.com>
2021-07-15 12:53:27 +0300

    Fix markup generating error

--- src/components/FaqSemanticMarkup/FaqSemanticMarkup.js
+++ src/components/FaqSemanticMarkup/FaqSemanticMarkup.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { FAQJsonLd } from 'gatsby-plugin-next-seo';
 
 import { parseString } from '@helpers';
 
 const FaqSemanticMarkup = ({ questions }) => {
-  const faqList = questions.map(({ title, content }) => {
-    return {
-      question: parseString(title.raw),
-      answer: parseString(content.raw),
-    };
-  });
+  const [faqList, setFaqList] = useState(null);
 
-  return <FAQJsonLd questions={faqList} />;
+  useEffect(() => {
+    const markupList = questions.map(({ title, content }) => {
+      return {
+        question: parseString(title.raw),
+        answer: parseString(content.raw),
+      };
+    });
+
+    setFaqList(markupList);
+  }, [questions]);
+
+  return faqList && <FAQJsonLd questions={faqList} />;
 };
 
 FaqSemanticMarkup.propTypes = {
