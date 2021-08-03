34df56deaf33fd23cc0d1e33a728ac9699420611
MityaDementiy <d.dementiy@gmail.com>
2021-08-03 12:59:02 +0300

    Set up pricing enterprise page

--- src/templates/PricingEnterpricePage.js
+++ src/templates/PricingEnterpricePage.js
@@ -1,222 +1,228 @@
-// import React from 'react';
-// import { graphql } from 'gatsby';
-// import PropTypes from 'prop-types';
+import React from 'react';
+import { graphql } from 'gatsby';
+import PropTypes from 'prop-types';
 
-// import EnterpricePage from '@scenes/EnterpricePage';
-// import Layout from '@components/Layout';
+import EnterpricePage from '@scenes/EnterpricePage';
+import Layout from '@components/Layout';
 
-// const Page = ({ data }) => {
-//   const testimonialsData = data.allPrismicHomepageBodyTestimonials.edges[0];
-//   if (!testimonialsData) return null;
-//   const testimonialsSection = testimonialsData.node;
+const Page = ({ data }) => {
+  const homePageData = data.allPrismicHomepage.edges[0].node.data.body;
+  const testimonialsSection = homePageData.find(
+    (section) => section.slice_type === 'testimonials'
+  );
 
-//   const worksData = data.allPrismicHomepageBodyWorks.edges[1];
-//   if (!worksData) return null;
-//   const worksSection = worksData.node;
+  const worksSection = homePageData.find(
+    (section) => section.slice_type === 'works'
+  );
 
-//   const enterpricepageContent = data.allPrismicPricesenterpricepage.edges[0];
-//   if (!enterpricepageContent) return null;
-//   const enterpricepage = enterpricepageContent.node;
+  const enterpricepageContent = data.allPrismicPricesenterpricepage.edges[0];
+  if (!enterpricepageContent) return null;
+  const enterpricepage = enterpricepageContent.node;
 
-//   const {
-//     uid,
-//     id,
-//     type,
-//     alternate_languages,
-//     lang,
-//     data: pageData,
-//   } = enterpricepage;
-//   const activeDocMeta = { id, uid, lang, type, alternate_languages };
-//   const { metatitle, metadescription, canonical } = pageData;
-//   const { body: pageContent } = pageData;
+  const {
+    uid,
+    id,
+    type,
+    alternate_languages,
+    lang,
+    data: pageData,
+  } = enterpricepage;
+  const activeDocMeta = { id, uid, lang, type, alternate_languages };
+  const { metatitle, metadescription, canonical } = pageData;
+  const { body: pageContent } = pageData;
 
-//   return (
-//     <Layout
-//       activeDocMeta={activeDocMeta}
-//       metatitle={metatitle}
-//       metadescription={metadescription}
-//       canonical={canonical}
-//     >
-//       <EnterpricePage
-//         content={pageContent}
-//         worksSection={worksSection}
-//         testimonialsSection={testimonialsSection}
-//         metatitle={metatitle}
-//         canonical={canonical}
-//       />
-//     </Layout>
-//   );
-// };
+  return (
+    <Layout
+      activeDocMeta={activeDocMeta}
+      metatitle={metatitle}
+      metadescription={metadescription}
+      canonical={canonical}
+    >
+      <EnterpricePage
+        content={pageContent}
+        worksSection={worksSection}
+        testimonialsSection={testimonialsSection}
+        metatitle={metatitle}
+        canonical={canonical}
+      />
+    </Layout>
+  );
+};
 
-// Page.propTypes = {
-//   data: PropTypes.object.isRequired,
-// };
+Page.propTypes = {
+  data: PropTypes.object.isRequired,
+};
 
-// export const query = graphql`
-//   query($uid: String, $lang: String) {
-//     allPrismicPricesenterpricepage(
-//       filter: { uid: { eq: $uid }, lang: { eq: $lang } }
-//     ) {
-//       edges {
-//         node {
-//           uid
-//           type
-//           lang
-//           id
-//           alternate_languages {
-//             id
-//             lang
-//             uid
-//             type
-//           }
-//           data {
-//             canonical {
-//               text
-//             }
-//             metadescription {
-//               text
-//             }
-//             metatitle {
-//               text
-//             }
-//             body {
-//               ... on PrismicPricesenterpricepageBodyFeature {
-//                 id
-//                 slice_type
-//                 primary {
-//                   title {
-//                     raw
-//                   }
-//                 }
-//                 items {
-//                   image {
-//                     alt
-//                     url
-//                   }
-//                   name {
-//                     raw
-//                   }
-//                 }
-//               }
-//               ... on PrismicPricesenterpricepageBodyHero {
-//                 id
-//                 slice_type
-//                 primary {
-//                   benefitslist {
-//                     raw
-//                   }
-//                   benefitstitle {
-//                     raw
-//                   }
-//                   ctatext {
-//                     raw
-//                   }
-//                   ctatitle {
-//                     raw
-//                   }
-//                   image {
-//                     alt
-//                     url
-//                   }
-//                   modalbuttonlink {
-//                     raw
-//                   }
-//                   modalbuttontext {
-//                     raw
-//                   }
-//                   subtitle {
-//                     raw
-//                   }
-//                   title {
-//                     raw
-//                   }
-//                   video {
-//                     link_type
-//                     url
-//                   }
-//                 }
-//               }
-//               ... on PrismicPricesenterpricepageBodyCallbanner {
-//                 id
-//                 slice_type
-//                 primary {
-//                   button {
-//                     raw
-//                   }
-//                   image {
-//                     alt
-//                     url
-//                   }
-//                   title {
-//                     raw
-//                   }
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     }
-//     allPrismicHomepageBodyTestimonials {
-//       edges {
-//         node {
-//           slice_type
-//           items {
-//             company {
-//               raw
-//             }
-//             name {
-//               raw
-//             }
-//             photo {
-//               alt
-//               url
-//             }
-//             text {
-//               raw
-//             }
-//           }
-//         }
-//       }
-//     }
-//     allPrismicHomepageBodyWorks {
-//       edges {
-//         node {
-//           slice_type
-//           primary {
-//             categories {
-//               text
-//             }
-//             dropdownlable {
-//               raw
-//             }
-//             title {
-//               raw
-//             }
-//           }
-//           items {
-//             category {
-//               text
-//             }
-//             link {
-//               link_type
-//               url
-//             }
-//             name {
-//               raw
-//             }
-//             screenshot {
-//               alt
-//               url
-//             }
-//             tag {
-//               raw
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `;
+export const query = graphql`
+  query($uid: String, $lang: String) {
+    allPrismicPricesenterpricepage(
+      filter: { uid: { eq: $uid }, lang: { eq: $lang } }
+    ) {
+      edges {
+        node {
+          uid
+          type
+          lang
+          id
+          alternate_languages {
+            id
+            lang
+            type
+            uid
+          }
+          data {
+            metatitle {
+              text
+            }
+            metadescription {
+              text
+            }
+            canonical {
+              text
+            }
+            body {
+              ... on PrismicPricesenterpricepageDataBodyHero {
+                id
+                slice_type
+                primary {
+                  video {
+                    link_type
+                    url
+                  }
+                  title {
+                    raw
+                  }
+                  subtitle {
+                    raw
+                  }
+                  modalbuttontext {
+                    raw
+                  }
+                  modalbuttonlink {
+                    raw
+                  }
+                  image {
+                    alt
+                    url
+                    gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+                  }
+                  ctatitle {
+                    raw
+                  }
+                  ctatext {
+                    raw
+                  }
+                  benefitstitle {
+                    raw
+                  }
+                  benefitslist {
+                    raw
+                  }
+                }
+              }
+              ... on PrismicPricesenterpricepageDataBodyFeature {
+                id
+                slice_type
+                primary {
+                  title {
+                    raw
+                  }
+                }
+                items {
+                  name {
+                    raw
+                  }
+                  image {
+                    alt
+                    url
+                  }
+                }
+              }
+              ... on PrismicPricesenterpricepageDataBodyCallbanner {
+                id
+                slice_type
+                primary {
+                  title {
+                    raw
+                  }
+                  image {
+                    alt
+                    url
+                  }
+                  button {
+                    raw
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    allPrismicHomepage {
+      edges {
+        node {
+          data {
+            body {
+              ... on PrismicHomepageDataBodyTestimonials {
+                id
+                slice_type
+                items {
+                  text {
+                    raw
+                  }
+                  photo {
+                    alt
+                    url
+                  }
+                  name {
+                    raw
+                  }
+                  company {
+                    raw
+                  }
+                }
+              }
+              ... on PrismicHomepageDataBodyWorks {
+                id
+                slice_type
+                primary {
+                  categories {
+                    text
+                  }
+                  dropdownlable {
+                    raw
+                  }
+                  title {
+                    raw
+                  }
+                }
+                items {
+                  category {
+                    text
+                  }
+                  link {
+                    link_type
+                    url
+                  }
+                  name {
+                    raw
+                  }
+                  tag {
+                    raw
+                  }
+                  screenshot {
+                    alt
+                    url
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
-// export default Page;
+export default Page;
