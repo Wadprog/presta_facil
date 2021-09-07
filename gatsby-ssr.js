3f1ec3a7b40ff344c8808b48a4dd35cfd4a14445
Manish Gautam <manish.gautam.india@gmail.com>
2021-09-07 19:12:24 +0530

    Revert staging envirnment script

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 export const onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
-    // <script
-    //   key="sp-app"
-    //   type="text/javascript"
-    //   src="https://app.secureprivacy.ai/script/606acb2d5761b5f013b48067.js"
-    // ></script>,
-
     <script
-    key="sp-app"
-    type="text/javascript"
-    src="https://frontend-staging.secureprivacy.ai/script/606acb2d5761b5f013b48067.js"
-  ></script>,
+      key="sp-app"
+      type="text/javascript"
+      src="https://app.secureprivacy.ai/script/606acb2d5761b5f013b48067.js"
+    ></script>,
+
+  //   <script
+  //   key="sp-app"
+  //   type="text/javascript"
+  //   src="https://frontend-staging.secureprivacy.ai/script/606acb2d5761b5f013b48067.js"
+  // ></script>,
 
 
   ]);
