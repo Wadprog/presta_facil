0405fee3d972ef0eb621a1752afbb290ca467f61
santiagosecureprivacy <santiago@secureprivacy.ai>
2022-03-02 17:06:44 -0300

    changes fr

--- src/components/Footer/components/Books/Books.js
+++ src/components/Footer/components/Books/Books.js
@@ -1,12 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Swiper from 'react-id-swiper';
 import { object } from 'prop-types';
 import { RichText } from 'prismic-reactjs';
 import { Link } from 'gatsby';
 
 import style from './Books.module.scss';
-import { langPath } from '@helpers';
-import LangContext from '@contexts';
 import Image from '@components/Image/Image';
 
 const DEFAULT_SLIDES = 3;
@@ -22,7 +20,6 @@ const Books = ({ data }) => {
       prevEl: '.prev',
     },
   };
-  const currentLang = useContext(LangContext);
 
   return (
     <div className={style.container}>
@@ -31,7 +28,7 @@ const Books = ({ data }) => {
         {items.map(({ image }, index) => {
           return (
             <div className={style.slide} key={`${image.url}${index}`}>
-              <Link to={langPath(currentLang) + '/books'}>
+              <Link to={'/books'}>
                 <Image image={image} key={image.url} />
               </Link>
             </div>
